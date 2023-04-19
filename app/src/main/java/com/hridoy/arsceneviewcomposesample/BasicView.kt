6c01eb0053ef5df87a931dcb5f477963bacf3b26
Hridoy Das <hridoycseboy@gmail.com>
2023-04-19 09:22:21 +0900

    AR SceneView with jetpack and xml experimental first commit

---
+++ app/src/main/java/com/hridoy/arsceneviewcomposesample/BasicView.kt
@@ -0,0 +1,11 @@
+package com.hridoy.arsceneviewcomposesample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BasicView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basic_view)
+    }
+}
