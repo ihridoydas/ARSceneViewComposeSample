ce956931bcc9e84bb617dc29461e9539da0cd288
Hridoy Das <hridoycseboy@gmail.com>
2023-04-19 17:27:10 +0900

    add xml AR Augmented Image

--- app/src/main/java/com/hridoy/arsceneviewcomposesample/DefaultActivity.kt
+++ app/src/main/java/com/hridoy/arsceneviewcomposesample/DefaultActivity.kt
@@ -1,29 +1,14 @@
 package com.hridoy.arsceneviewcomposesample
 
-import android.content.Context
-import android.os.Build
 import android.os.Bundle
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.core.view.isGone
-import com.google.ar.core.TrackingFailureReason
-import io.github.sceneview.ar.ArSceneView
-import io.github.sceneview.ar.node.ArModelNode
-import io.github.sceneview.ar.node.PlacementMode
+import androidx.fragment.app.commit
 import io.github.sceneview.utils.doOnApplyWindowInsets
 import io.github.sceneview.utils.setFullScreen
 
 class DefaultActivity : AppCompatActivity(R.layout.activity_basic_view) {
-
-    lateinit var sceneView: ArSceneView
-    lateinit var loadingView: View
-    lateinit var statusText: TextView
-
-    var modelNode: ArModelNode? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -41,36 +26,9 @@ class DefaultActivity : AppCompatActivity(R.layout.activity_basic_view) {
             title = ""
         })
 
-        modelNode?.detachAnchor()
-        modelNode?.placementMode = PlacementMode.BEST_AVAILABLE
-
-
-
-        //Tracking ちゃんとできない場合
-        fun TrackingFailureReason.getDescription(context: Context) = when(this) {
-            TrackingFailureReason.NONE -> ""
-            TrackingFailureReason.BAD_STATE -> context.getString(R.string.sceneview_bad_state_message)
-            TrackingFailureReason.INSUFFICIENT_LIGHT -> context.getString(
-                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
-                    R.string.sceneview_insufficient_light_message
-                } else {
-                    R.string.sceneview_insufficient_light_android_s_message
-                }
-            )
-            TrackingFailureReason.EXCESSIVE_MOTION -> context.getString(R.string.sceneview_excessive_motion_message)
-            TrackingFailureReason.INSUFFICIENT_FEATURES -> context.getString(R.string.sceneview_insufficient_features_message)
-            TrackingFailureReason.CAMERA_UNAVAILABLE -> context.getString(R.string.sceneview_camera_unavailable_message)
-            else -> context.getString(R.string.sceneview_unknown_tracking_failure, this)
+        supportFragmentManager.commit {
+            add(R.id.containerFragment, MainFragment::class.java, Bundle())
         }
-
-        statusText = findViewById(R.id.statusText)
-        sceneView = findViewById<ArSceneView?>(R.id.sceneView).apply {
-            onArTrackingFailureChanged = { reason ->
-                statusText.text = reason?.getDescription(context)
-                statusText.isGone = reason == null
-            }
-        }
-        loadingView = findViewById(R.id.loadingView)
 
     }
 }
