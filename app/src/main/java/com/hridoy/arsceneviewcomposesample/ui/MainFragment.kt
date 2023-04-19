ce267859a0a14466e06037ec897142e51dd54b7f
Hridoy Das <hridoycseboy@gmail.com>
2023-04-19 15:38:54 +0900

    Experiment AR cursor Placement

--- app/src/main/java/com/hridoy/arsceneviewcomposesample/ui/MainFragment.kt
+++ app/src/main/java/com/hridoy/arsceneviewcomposesample/ui/MainFragment.kt
@@ -1,52 +1,137 @@
 package com.hridoy.arsceneviewcomposesample.ui
 
+import android.media.MediaRecorder
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import com.google.ar.core.Anchor
 import com.hridoy.arsceneviewcomposesample.R
-import io.github.sceneview.SceneView
-import io.github.sceneview.loaders.loadHdrIndirectLight
-import io.github.sceneview.loaders.loadHdrSkybox
+import io.github.sceneview.ar.ArSceneView
+import io.github.sceneview.ar.node.ArModelNode
+import io.github.sceneview.ar.node.CursorNode
 import io.github.sceneview.math.Position
-import io.github.sceneview.math.Rotation
-import io.github.sceneview.nodes.ModelNode
+import io.github.sceneview.utils.doOnApplyWindowInsets
 
 class MainFragment : Fragment(R.layout.fragment_main) {
-    
-    lateinit var sceneView : SceneView
-    lateinit var loading : View
+
+    lateinit var sceneView: ArSceneView
+    lateinit var loadingView: View
+    lateinit var anchorButton: ExtendedFloatingActionButton
+    lateinit var recordButton: ExtendedFloatingActionButton
+
+    lateinit var cursorNode: CursorNode
+    lateinit var modelNode: ArModelNode
+
+    var isLoading = false
+        set(value) {
+            field = value
+            loadingView.isGone = !value
+            anchorButton.isGone = value
+            recordButton.isGone = value
+        }
+
+    val fileName by lazy { "${requireContext().externalCacheDir?.absolutePath}/screen_record.mp4" }
+
+    lateinit var recorder: MediaRecorder
+
+    var isRecording = false
+        set(value) {
+            field = value
+            recordButton.setText(if (value) R.string.stop else R.string.record)
+        }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        sceneView = view.findViewById<SceneView>(R.id.sceneView).apply {
-            setLifecycle(lifecycle = lifecycle)
+
+        loadingView = view.findViewById(R.id.loadingView)
+        anchorButton = view.findViewById<ExtendedFloatingActionButton>(R.id.anchorButton).apply {
+            val bottomMargin = (layoutParams as ViewGroup.MarginLayoutParams).bottomMargin
+            doOnApplyWindowInsets { systemBarsInsets ->
+                (layoutParams as ViewGroup.MarginLayoutParams).bottomMargin =
+                    systemBarsInsets.bottom + bottomMargin
+            }
+            setOnClickListener { cursorNode.createAnchor()?.let { anchorOrMove(it) } }
+        }
+        recordButton = view.findViewById<ExtendedFloatingActionButton>(R.id.recordButton).apply {
+            setOnClickListener {
+                isRecording = if (isRecording) {
+                    stopRecording()
+                    false
+                } else {
+                    startRecording()
+                    true
+                }
+            }
         }
 
-        loading = view.findViewById(R.id.loadingView)
-
-        lifecycleScope.launchWhenCreated {
-            val hdrFile = "environments/studio_small_09_2k.hdr"
-            sceneView.loadHdrIndirectLight(hdrFile, specularFilter = true){
-                intensity(30_000f)
+        sceneView = view.findViewById<ArSceneView?>(R.id.sceneView).apply {
+            planeRenderer.isVisible = false
+            // Handle a fallback in case of non AR usage. The exception contains the failure reason
+            // e.g. SecurityException in case of camera permission denied
+            onArSessionFailed = { _: Exception ->
+                // If AR is not available or the camara permission has been denied, we add the model
+                // directly to the scene for a fallback 3D only usage
+                modelNode.centerModel(origin = Position(x = 0.0f, y = 0.0f, z = 0.0f))
+                modelNode.scaleModel(units = 1.0f)
+                sceneView.addChild(modelNode)
             }
-            sceneView.loadHdrSkybox(hdrFile){
-                intensity(50_000f)
+            onTapAr = { hitResult, _ ->
+                anchorOrMove(hitResult.createAnchor())
             }
-
-            val model = sceneView.modelLoader.loadModel("models/MaterialSuite.glb")!!
-            val modelNode = ModelNode(sceneView,model).apply {
-                transform(
-                    position = Position(z= -4.0f),
-                    rotation = Rotation(x = 15.0f)
-                )
-                scaleToUnitsCube(2.0f)
-                //                centerOrigin(Position(x=-1.0f, y=-1.0f))
-                playAnimation()
-            }
-            sceneView.addChildNode(modelNode)
-            loading.isGone = true
         }
+
+        cursorNode = CursorNode().apply {
+            onHitResult = { node, _ ->
+                if (!isLoading) {
+                    anchorButton.isGone = !node.isTracking
+                }
+            }
+        }
+        sceneView.addChild(cursorNode)
+
+        isLoading = true
+        modelNode = ArModelNode(
+            modelGlbFileLocation = "models/spiderbot.glb",
+            onLoaded = { modelInstance ->
+                anchorButton.text = getString(R.string.move_object)
+                anchorButton.setIconResource(R.drawable.ic_target)
+                isLoading = false
+            })
     }
-    
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopRecording()
+    }
+
+    fun anchorOrMove(anchor: Anchor) {
+        if (!sceneView.children.contains(modelNode)) {
+            sceneView.addChild(modelNode)
+        }
+        modelNode.anchor = anchor
+    }
+
+    //Bug issue by Oppo Device
+    //https://github.com/google-ar/arcore-android-sdk/issues/1261
+    fun startRecording() {
+        recorder = MediaRecorder().apply {
+            setVideoSource(MediaRecorder.VideoSource.SURFACE)
+            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+            setOutputFile(fileName)
+            setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT)
+            setVideoSize(sceneView.width, sceneView.height)
+            prepare()
+        }
+        recorder.start()
+        sceneView.startMirroring(recorder.surface)
+    }
+
+    private fun stopRecording() {
+        sceneView.stopMirroring(recorder.surface)
+        recorder.stop()
+        recorder.release()
+    }
 }
