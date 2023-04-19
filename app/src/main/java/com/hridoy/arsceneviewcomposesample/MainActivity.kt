b86b755ac820873b14a6b4d928ca2e17e6709018
Hridoy Das <hridoycseboy@gmail.com>
2023-04-19 14:37:19 +0900

    Experiment and practise done (SceneView Model Viewer)

--- app/src/main/java/com/hridoy/arsceneviewcomposesample/MainActivity.kt
+++ app/src/main/java/com/hridoy/arsceneviewcomposesample/MainActivity.kt
@@ -1,25 +1,19 @@
 package com.hridoy.arsceneviewcomposesample
 
 import android.os.Bundle
-import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.hridoy.arsceneviewcomposesample.ui.theme.ARSceneViewComposeSampleTheme
 import io.github.sceneview.ar.ARScene
-import io.github.sceneview.ar.ArSceneView
-import io.github.sceneview.ar.node.ArModelNode
 import io.github.sceneview.ar.node.ArNode
-import io.github.sceneview.ar.node.CursorNode
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,23 +37,23 @@ fun ARScreen() {
     val nodes = remember { mutableStateListOf<ArNode>() }
 
 
-//    Box(modifier = Modifier.fillMaxSize()) {
-//        ARScene(
-//            modifier = Modifier.fillMaxSize(),
-//            nodes = nodes,
-//            planeRenderer = true,
-//            onCreate = { arSceneView ->
-//                // Apply your configuration
-//            },
-//            onSessionCreate = { session ->
-//                // Configure the ARCore session
-//            },
-//            onFrame = { arFrame ->
-//                // Retrieve ARCore frame update
-//            },
-//            onTap = { hitResult ->
-//                // User tapped in the AR view
-//            }
-//        )
-//    }
+    Box(modifier = Modifier.fillMaxSize()) {
+        ARScene(
+            modifier = Modifier.fillMaxSize(),
+            nodes = nodes,
+            planeRenderer = true,
+            onCreate = { arSceneView ->
+                // Apply your configuration
+            },
+            onSessionCreate = { session ->
+                // Configure the ARCore session
+            },
+            onFrame = { arFrame ->
+                // Retrieve ARCore frame update
+            },
+            onTap = { hitResult ->
+                // User tapped in the AR view
+            }
+        )
+    }
 }
