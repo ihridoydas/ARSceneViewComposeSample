2f71bbbc21dcc364da6204ba925fc6c18ee6cdbb
Hridoy Das <hridoycseboy@gmail.com>
2023-04-21 15:08:52 +0900

    AR Light Estimate xml environmental experiment done

--- app/src/main/java/com/hridoy/arsceneviewcomposesample/DefaultActivity.kt
+++ app/src/main/java/com/hridoy/arsceneviewcomposesample/DefaultActivity.kt
@@ -1,76 +1,117 @@
 package com.hridoy.arsceneviewcomposesample
 
-import android.content.Context
-import android.os.Build
+import android.net.Uri
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import android.view.MotionEvent
+import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.core.view.isGone
-import com.google.ar.core.TrackingFailureReason
-import io.github.sceneview.ar.ArSceneView
-import io.github.sceneview.ar.node.ArModelNode
-import io.github.sceneview.ar.node.PlacementMode
-import io.github.sceneview.utils.doOnApplyWindowInsets
-import io.github.sceneview.utils.setFullScreen
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.commit
+import androidx.lifecycle.lifecycleScope
+import com.google.android.filament.utils.HDRLoader
+import com.google.ar.core.Config
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.Scene
+import com.google.ar.sceneform.math.Vector3
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.Renderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+import com.gorisse.thomas.sceneform.environment
+import com.gorisse.thomas.sceneform.environment.loadEnvironment
+import com.gorisse.thomas.sceneform.light.LightEstimationConfig
+import com.gorisse.thomas.sceneform.lightEstimationConfig
+import com.hridoy.arsceneviewcomposesample.databinding.ActivityBasicViewBinding
 
-class DefaultActivity : AppCompatActivity(R.layout.activity_basic_view) {
+const val serverUrl = "https://sceneview.github.io/assets"
+class DefaultActivity : AppCompatActivity() {
 
-    lateinit var sceneView: ArSceneView
-    lateinit var loadingView: View
-    lateinit var statusText: TextView
+    private lateinit var binding: ActivityBasicViewBinding
 
-    var modelNode: ArModelNode? = null
+    private lateinit var arFragment: ArFragment
+    private val sceneView: com.google.ar.sceneform.ArSceneView get() = arFragment.arSceneView
+    private val scene: Scene get() = sceneView.scene
+
+    var model: Renderable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setFullScreen(
-            findViewById(R.id.rootView),
-            fullScreen = true,
-            hideSystemBars = false,
-            fitsSystemWindows = false
-        )
-
-        setSupportActionBar(findViewById<Toolbar>(R.id.toolbar)?.apply {
-            doOnApplyWindowInsets { systemBarsInsets ->
-                (layoutParams as ViewGroup.MarginLayoutParams).topMargin = systemBarsInsets.top
+        binding = DataBindingUtil.setContentView<ActivityBasicViewBinding>(this, R.layout.activity_basic_view)
+            .apply {
+                lifecycleOwner = this@DefaultActivity
+                activity = this@DefaultActivity
             }
-            title = ""
-        })
+        setSupportActionBar(binding.toolbar)
 
-        modelNode?.detachAnchor()
-        modelNode?.placementMode = PlacementMode.BEST_AVAILABLE
+        supportFragmentManager.addFragmentOnAttachListener { _, fragment ->
+            (fragment as? ArFragment)?.let { onArFragmentAttached(it) }
+        }
+        supportFragmentManager.commit {
+            add(R.id.arFragment, ArFragment::class.java, Bundle().apply {
+                putBoolean(ArFragment.ARGUMENT_FULLSCREEN, false)
+            })
+        }
 
-
-
-        //Tracking ちゃんとできない場合
-        fun TrackingFailureReason.getDescription(context: Context) = when(this) {
-            TrackingFailureReason.NONE -> ""
-            TrackingFailureReason.BAD_STATE -> context.getString(R.string.sceneview_bad_state_message)
-            TrackingFailureReason.INSUFFICIENT_LIGHT -> context.getString(
-                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
-                    R.string.sceneview_insufficient_light_message
-                } else {
-                    R.string.sceneview_insufficient_light_android_s_message
-                }
+        lifecycleScope.launchWhenResumed {
+            sceneView.environment = HDRLoader.loadEnvironment(
+                this@DefaultActivity,
+                "$serverUrl/environments/evening_meadow_2k.hdr"
             )
-            TrackingFailureReason.EXCESSIVE_MOTION -> context.getString(R.string.sceneview_excessive_motion_message)
-            TrackingFailureReason.INSUFFICIENT_FEATURES -> context.getString(R.string.sceneview_insufficient_features_message)
-            TrackingFailureReason.CAMERA_UNAVAILABLE -> context.getString(R.string.sceneview_camera_unavailable_message)
-            else -> context.getString(R.string.sceneview_unknown_tracking_failure, this)
+            ModelRenderable.builder()
+                .setSource(
+                    this@DefaultActivity,
+                    Uri.parse("$serverUrl/models/ClearCoat.glb")
+                )
+                .setIsFilamentGltf(true)
+                .build()
+                .thenAccept {
+                    model = it
+                }
+        }
+    }
+
+    fun onArFragmentAttached(fragment: ArFragment) {
+        arFragment = fragment
+        arFragment.setOnTapArPlaneListener(::onTapPlane)
+    }
+
+    fun onTapPlane(hitResult: HitResult, plane: Plane, motionEvent: MotionEvent) {
+        if (model == null) {
+            toast("Loading...")
+            return
         }
 
-        statusText = findViewById(R.id.statusText)
-        sceneView = findViewById<ArSceneView?>(R.id.sceneView).apply {
-            onArTrackingFailureChanged = { reason ->
-                statusText.text = reason?.getDescription(context)
-                statusText.isGone = reason == null
+        val anchorNode = AnchorNode(hitResult.createAnchor()).apply {
+            localScale = Vector3(0.05f, 0.05f, 0.05f)
+        }
+        TransformableNode(arFragment.transformationSystem).apply {
+            setParent(anchorNode)
+            setRenderable(model)
+                .animate(true).start()
+        }
+        scene.addChild(anchorNode)
+    }
+
+    fun onEstimationModeChanged(button: CompoundButton?, checked: Boolean) {
+        if (checked) {
+            button?.id?.let { estimationMode ->
+                sceneView.lightEstimationConfig = LightEstimationConfig(
+                    mode = when (estimationMode) {
+                       R.id.environmentalHdrMode,
+                       R.id.environmentalHdrNoReflections,
+                       R.id.environmentalHdrNoSpecularFilter -> {
+                            Config.LightEstimationMode.ENVIRONMENTAL_HDR
+                        }
+                        R.id.ambientIntensityMode -> Config.LightEstimationMode.AMBIENT_INTENSITY
+                        else -> Config.LightEstimationMode.DISABLED
+                    },
+                    environmentalHdrReflections = estimationMode != R.id.environmentalHdrNoReflections,
+                    environmentalHdrSpecularFilter = estimationMode != R.id.environmentalHdrNoSpecularFilter
+                )
             }
         }
-        loadingView = findViewById(R.id.loadingView)
-
     }
 }
