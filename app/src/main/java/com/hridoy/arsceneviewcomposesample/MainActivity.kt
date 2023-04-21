dbbcab75a6d87fdec9c79f5010db3f49564b9235
Hridoy Das <hridoycseboy@gmail.com>
2023-04-21 14:30:03 +0900

    some ui change

--- app/src/main/java/com/hridoy/arsceneviewcomposesample/MainActivity.kt
+++ app/src/main/java/com/hridoy/arsceneviewcomposesample/MainActivity.kt
@@ -4,14 +4,21 @@ import android.app.Activity
 import android.app.ActivityManager
 import android.content.Context
 import android.content.pm.ActivityInfo
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.widget.HorizontalScrollView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,27 +26,40 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.core.view.isGone
+import androidx.lifecycle.lifecycleScope
 import com.google.accompanist.systemuicontroller.SystemUiController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.google.android.filament.utils.HDRLoader
 import com.google.ar.core.Anchor
+import com.google.ar.core.Config
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.Renderable
 import com.hridoy.arsceneviewcomposesample.ui.theme.ARSceneViewComposeSampleTheme
 import io.github.sceneview.ar.ARScene
 import io.github.sceneview.ar.ArSceneView
+import io.github.sceneview.ar.arcore.LightEstimationMode
 import io.github.sceneview.ar.getDescription
 import io.github.sceneview.ar.node.ArModelNode
 import io.github.sceneview.ar.node.ArNode
 import io.github.sceneview.ar.node.CursorNode
 import io.github.sceneview.ar.node.PlacementMode
+import io.github.sceneview.environment.loadEnvironment
 import io.github.sceneview.math.Position
+import kotlinx.coroutines.launch
 import java.util.Objects
 
+const val serverUrl = "https://sceneview.github.io/assets"
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
@@ -70,7 +90,11 @@ fun ARScreen() {
 
     lateinit var arModelNode: ArModelNode
     lateinit var cursorNode: CursorNode
-    lateinit var arSceneView: ArSceneView
+
+    val context= LocalContext.current
+    var model: Renderable? = null
+
+    val scope = rememberCoroutineScope()
 
     var status by remember { mutableStateOf("") }
     Box(
@@ -84,25 +108,35 @@ fun ARScreen() {
                 // Apply your configuration
                 arSceneView.setupAvConfigurations()
 
-                arSceneView?.apply {
-                    planeRenderer.isVisible = false
-                    onArTrackingFailureChanged = { reason ->
-                        status = reason?.getDescription(context).toString()
+                //Light Estimate
+                scope.launch {
+                    arSceneView.environment = HDRLoader.loadEnvironment(
+                        context,
+                        "$serverUrl/environments/evening_meadow_2k.hdr"
+                    )
+
+                    arSceneView?.apply {
+                        planeRenderer.isVisible = false
+                        onArTrackingFailureChanged = { reason ->
+                            status = reason?.getDescription(context).toString()
+
+                        }
+                        onArSessionFailed = { _: Exception ->
+                            // If AR is not available or the camara permission has been denied, we add the model
+                            // directly to the scene for a fallback 3D only usage
+                            arModelNode.centerModel(origin = Position(x = 0.0f, y = 0.0f, z = 0.0f))
+                            arModelNode.scaleModel(units = 1.0f)
+                            arSceneView.addChild(arModelNode)
+                        }
+                        onTapAr = { hitResult, _ ->
+                            anchorOrMove(hitResult.createAnchor(), arModelNode = arModelNode, arSceneView = arSceneView)
+                        }
+
 
                     }
-                    onArSessionFailed = { _: Exception ->
-                        // If AR is not available or the camara permission has been denied, we add the model
-                        // directly to the scene for a fallback 3D only usage
-                        arModelNode.centerModel(origin = Position(x = 0.0f, y = 0.0f, z = 0.0f))
-                        arModelNode.scaleModel(units = 1.0f)
-                        arSceneView.addChild(arModelNode)
-                    }
-                    onTapAr = { hitResult, _ ->
-                        anchorOrMove(hitResult.createAnchor(), arModelNode = arModelNode, arSceneView = arSceneView)
-                    }
-
-
                 }
+
+
 
 
 //                arModelNode =
@@ -139,11 +173,6 @@ fun ARScreen() {
                     onLoaded = { modelInstance ->
 
                     })
-
-
-
-
-
             },
             onSessionCreate = { session ->
                 // Configure the ARCore session
@@ -163,12 +192,49 @@ fun ARScreen() {
                 .background(Color.White)
                 .align(Alignment.Center),
         )
+        val scrollState = rememberScrollState()
 
-        Button(onClick = {
-            cursorNode.createAnchor()?.let { anchorOrMove(anchor = it, arModelNode = arModelNode, arSceneView = arSceneView) }
-        }, modifier = Modifier.align(Alignment.BottomCenter)) {
-            Text(text = "Set Anchor")
+        Row(modifier = Modifier
+            .horizontalScroll(state = scrollState)
+            .align(Alignment.BottomCenter)
+            .padding(5.dp)
+            .padding(bottom = 40.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Button(onClick = {
+                Config.LightEstimationMode.ENVIRONMENTAL_HDR
+            }, modifier = Modifier) {
+                Text(text = "ENVIRONMENTAL_HDR")
+            }
+            Button(onClick = {
+                Config.LightEstimationMode.DISABLED
+            }, modifier = Modifier) {
+                Text(text = "ENVIRONMENTAL_HDR_FAKE_REFLECTIONS")
+            }
+            Button(onClick = {
+
+                Config.LightEstimationMode.AMBIENT_INTENSITY
+            }, modifier = Modifier) {
+                Text(text = "ENVIRONMENTAL_HDR_NO_REFLECTIONS")
+            }
+
+            Button(onClick = {
+
+            }, modifier = Modifier) {
+                Text(text = "AMBIENT_INTENSITY")
+            }
+            Button(onClick = {
+
+            }, modifier = Modifier) {
+                Text(text = "DISABLED")
+            }
+            Button(onClick = {
+                    //cursorNode.createAnchor()?.let { anchorOrMove(anchor = it, arModelNode = arModelNode, arSceneView = arSceneView) }
+            }, modifier = Modifier) {
+                Text(text = "Set Anchor")
+            }
         }
+
+
     }
 }
 
