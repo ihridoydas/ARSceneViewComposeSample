1179b283a7c061ff2f3f0e2a97a93390021f86d8
Hridoy Chandra Das <hridoy@qualias.jp>
2024-02-04 20:53:26 +0900

    UI Update Ar with compose

--- app/src/main/java/com/hridoy/arsceneviewcomposesample/AvConfig.kt
+++ app/src/main/java/com/hridoy/arsceneviewcomposesample/AvConfig.kt
@@ -17,23 +17,41 @@ fun ArSceneView.setupAvConfigurations() {
             AugmentedImageDatabase.deserialize(arSession, database)
         }
     }
+}
+//Tracking ちゃんとできない場合
+//Tracking ちゃんとできない場合
+fun TrackingFailureReason.getDescription(context: Context) = when(this) {
+    TrackingFailureReason.NONE -> ""
+    TrackingFailureReason.BAD_STATE -> context.getString(R.string.sceneview_bad_state_message)
+    TrackingFailureReason.INSUFFICIENT_LIGHT -> context.getString(
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
+            R.string.sceneview_insufficient_light_message
+        } else {
+            R.string.sceneview_insufficient_light_android_s_message
+        }
+    )
+    TrackingFailureReason.EXCESSIVE_MOTION -> context.getString(R.string.sceneview_excessive_motion_message)
+    TrackingFailureReason.INSUFFICIENT_FEATURES -> context.getString(R.string.sceneview_insufficient_features_message)
+    TrackingFailureReason.CAMERA_UNAVAILABLE -> context.getString(R.string.sceneview_camera_unavailable_message)
+    else -> context.getString(R.string.sceneview_unknown_tracking_failure, this)
+}
 
-
-    //Tracking ちゃんとできない場合
-    fun TrackingFailureReason.getDescription(context: Context) = when (this) {
-        TrackingFailureReason.NONE -> ""
-        TrackingFailureReason.BAD_STATE -> context.getString(R.string.sceneview_bad_state_message)
-        TrackingFailureReason.INSUFFICIENT_LIGHT -> context.getString(
-            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
-                R.string.sceneview_insufficient_light_message
-            } else {
-                R.string.sceneview_insufficient_light_android_s_message
-            }
-        )
-
-        TrackingFailureReason.EXCESSIVE_MOTION -> context.getString(R.string.sceneview_excessive_motion_message)
-        TrackingFailureReason.INSUFFICIENT_FEATURES -> context.getString(R.string.sceneview_insufficient_features_message)
-        TrackingFailureReason.CAMERA_UNAVAILABLE -> context.getString(R.string.sceneview_camera_unavailable_message)
-        else -> context.getString(R.string.sceneview_unknown_tracking_failure, this)
+const val NONE = "NONE"
+const val BAD_STATE = "BAD_STATE"
+const val INSUFFICIENT_LIGHT = "INSUFFICIENT_LIGHT"
+const val EXCESSIVE_MOTION = "EXCESSIVE_MOTION"
+const val INSUFFICIENT_FEATURES = "INSUFFICIENT_FEATURES"
+const val CAMERA_UNAVAILABLE = "CAMERA_UNAVAILABLE"
+fun setupTrackingFailureChanged(trackingFailureReason: TrackingFailureReason) {
+    trackingFailureReason.apply {
+        when (this) {
+            TrackingFailureReason.NONE -> NONE
+            TrackingFailureReason.BAD_STATE -> BAD_STATE
+            TrackingFailureReason.INSUFFICIENT_LIGHT -> INSUFFICIENT_LIGHT
+            TrackingFailureReason.EXCESSIVE_MOTION -> EXCESSIVE_MOTION
+            TrackingFailureReason.INSUFFICIENT_FEATURES -> INSUFFICIENT_FEATURES
+            TrackingFailureReason.CAMERA_UNAVAILABLE -> CAMERA_UNAVAILABLE
+        }
     }
 }
+
--- app/src/main/java/com/hridoy/arsceneviewcomposesample/MainActivity.kt
+++ app/src/main/java/com/hridoy/arsceneviewcomposesample/MainActivity.kt
@@ -5,16 +5,25 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
@@ -40,18 +49,42 @@ fun AvApp(
     mainViewModel: MainViewModel = viewModel(),
 ) {
     val node by mainViewModel.node.collectAsState()
-
-    ARScene(
-        modifier = Modifier.fillMaxSize(),
-        nodes = node?.let { listOf(it) } ?: emptyList(),
-        planeRenderer = true,
-        onCreate = {
-            it.setupAvConfigurations()
-        },
-        onFrame = {
-            mainViewModel.renderVideo(it.updatedAugmentedImages)
+    //Screen Status
+    var status by remember { mutableStateOf("") }
+    Box(modifier = Modifier.fillMaxSize()) {
+        ARScene(
+            modifier = Modifier.fillMaxSize(),
+            nodes = node?.let { listOf(it) } ?: emptyList(),
+            planeRenderer = true,
+            onCreate = {
+                it.setupAvConfigurations()
+            },
+            onFrame = {
+                mainViewModel.renderVideo(it.updatedAugmentedImages)
+            },
+            onTrackingFailureChanged = { trackingFailureReason ->
+                //Tracking ちゃんとできない場合
+                onArTrackingFailureChanged = { reason ->
+                    status = reason?.apply {
+                        setupTrackingFailureChanged(trackingFailureReason = this)
+                    }.toString()
+                }
+            }
+        )
+        //Middle Section
+        Box(modifier = Modifier
+            .fillMaxSize(), contentAlignment = Alignment.Center) {
+            if (status.isNotEmpty()) {
+                Text(
+                    text = if(status.equals("null")) "" else status,
+                    modifier = Modifier
+                        .padding(16.dp),
+                    color = MaterialTheme.colorScheme.background,
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
         }
-    )
+    }
 }
 
 @Preview
